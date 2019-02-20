93fcddf5bd5c2473b8e8186b3e575893f6b3b448
Maquebax <vicky.evileye@gmail.com>
2019-02-20 11:44:30 +0530

    minor addition to code

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+K
+ApplicationContextAware.iml,1\4\148f443e2feb4e91a42621679c00fe2aafc6a5f8
--- src/com/maquebax/applicationawareex/MainApp.java
+++ src/com/maquebax/applicationawareex/MainApp.java
@@ -11,6 +11,7 @@ public class MainApp {
         System.out.println("***************** date retrieved from singleton bean 1 "+obj.getsDate());
         Thread.sleep(1000);
         System.out.println("\n\n");
+        obj  = (SingletonClass)ctx.getBean("singletonClass");
         System.out.println("***************** date retrieved from singleton bean using appctx aware 2 "+obj.getpDate());
         System.out.println("***************** date retrieved from singleton bean 1 "+obj.getsDate());
     }
